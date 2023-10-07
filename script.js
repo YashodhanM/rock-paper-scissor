3399f4524d2cc5d7f49c74ba1d4bd6590dd6ca69
YashodhanM <yashodhanmohite101@gmail.com>
2023-10-07 20:08:13 +0530

    Code completed: Everything works as expected. Code shows Player's win, Computer's win and keeps the count of who won the most ,and displays it. :)

--- script.js
+++ script.js
@@ -11,15 +11,15 @@ function playRound(playerSelection, computerSelection){
     if(playerSelection == "rock" && computerSelection == "paper"){
         return "You lose! Paper beats Rock!";
     }else if(playerSelection == "rock" && computerSelection == "scissor"){
-        return "You Win! Rock beats Scissor!";
+        return "You win! Rock beats Scissor!";
     }else if(playerSelection == "paper" && computerSelection == "scissor"){
         return "You lose! Scissor beats paper!";
     }else if(playerSelection == "paper" && computerSelection == "rock"){
-        return "You Win! Paper beats rock!";
+        return "You win! Paper beats rock!";
     }else if(playerSelection == "scissor" && computerSelection == "rock"){
         return "You lose! Rock beats scissor!";
     }else if(playerSelection == "scissor" && computerSelection == "paper"){
-        return "You Win! Scissor beats paper!";
+        return "You win! Scissor beats paper!";
     }else{
         return "There was a tie!";
     }
@@ -28,12 +28,33 @@ function playRound(playerSelection, computerSelection){
 function game(){
     let playerSelection = "";
     let computerSelection = "";
+    let myResult = "";
     let playerCount = 0;
     let computerCount = 0;
     for(let i = 1; i <= 5; i++){
         playerSelection = prompt("Enter a value of you liking to beat the computer: ");
         computerSelection = getComputerChoice();
-        playRound(playerSelection,computerSelection);
+        myResult = playRound(playerSelection,computerSelection);
+        console.log(myResult);
+        if(myResult.includes("win")){
+            playerCount += 1;
+        }else if(myResult.includes("lose")){
+            computerCount += 1;
+        }else{
+            playerCount += 0;
+            computerCount += 0;
+        }
+    }
+
+    console.log(`Player Wins: ${playerCount}`);
+    console.log(`Computer Wins: ${computerCount}`);
+
+    if(playerCount > computerCount){
+        console.log("You Win this battle!!!");
+    }else if(playerCount < computerCount){
+        console.log("You lost this battle :(");
+    }else{
+        console.log("There was a tie!!!");
     }
 }
 
