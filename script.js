6167c1de56df238428803edaa03c4de1b787831a
YashodhanM <yashodhanmohite101@gmail.com>
2023-10-07 19:54:15 +0530

    Function playRound: Made changes in this function. Mainly changed the results from console.log() to return statements.

--- script.js
+++ script.js
@@ -9,31 +9,32 @@ function playRound(playerSelection, computerSelection){
     computerSelection = computerSelection.toLowerCase();
 
     if(playerSelection == "rock" && computerSelection == "paper"){
-        console.log("You lose! Paper beats Rock!");
+        return "You lose! Paper beats Rock!";
     }else if(playerSelection == "rock" && computerSelection == "scissor"){
-        console.log("You Win! Rock beats Scissor!");
+        return "You Win! Rock beats Scissor!";
     }else if(playerSelection == "paper" && computerSelection == "scissor"){
-        console.log("You lose! Scissor beats paper!");
+        return "You lose! Scissor beats paper!";
     }else if(playerSelection == "paper" && computerSelection == "rock"){
-        console.log("You Win! Paper beats rock!");
+        return "You Win! Paper beats rock!";
     }else if(playerSelection == "scissor" && computerSelection == "rock"){
-        console.log("You lose! Rock beats scissor!");
+        return "You lose! Rock beats scissor!";
     }else if(playerSelection == "scissor" && computerSelection == "paper"){
-        console.log("You Win! Scissor beats paper!");
+        return "You Win! Scissor beats paper!";
     }else{
-        console.log("There was a tie!");
+        return "There was a tie!";
     }
 }
 
 function game(){
     let playerSelection = "";
     let computerSelection = "";
+    let playerCount = 0;
+    let computerCount = 0;
     for(let i = 1; i <= 5; i++){
         playerSelection = prompt("Enter a value of you liking to beat the computer: ");
         computerSelection = getComputerChoice();
         playRound(playerSelection,computerSelection);
     }
-
 }
 
 game();
